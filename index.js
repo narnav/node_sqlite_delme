06b573ba7e6c6aaf19e491a10fe9737be83dbee8
narnav <narnavg@gmail.com>
2025-12-17 13:10:25 +0200

    upd

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 import sqlite3 from "sqlite3";
+import promptSync from "prompt-sync";
+const prompt = promptSync();
 
-const db = new sqlite3.Database("database.db", (err) => {
-    if (err) {
-        return console.error("DB Connection Error:", err.message);
-    }
-    console.log("Connected to SQLite database.");
+export const Selctions = Object.freeze({
+    ADD: 1,
+    DISPLAY: 2,
+    EXIT: 3,
+    DISPLAY_SINGLE: 4,
 });
 
-const init_db = () => {
+export const menu = () => {
+    let selections_ar = Object.keys(Selctions)
+    console.log("Welcome to my app...");
+    for (let index = 0; index < selections_ar.length; index++) {
+        console.log(`${index + 1} - ${selections_ar[index]}`)
+    }
+
+    const user_selection = prompt("your selection?");
+    return Number(user_selection);
+}
+
+let db
+const init_db = async () => {
+    db = new sqlite3.Database("database.db", (err) => {
+        if (err) {
+            return console.error("DB Connection Error:", err.message);
+        }
+        console.log("Connected to SQLite database.");
+    });
+}
+init_db()
+
+const init_table = () => {
     // Example: Create table
     db.run(
         `CREATE TABLE IF NOT EXISTS users (
@@ -22,12 +46,34 @@ const init_db = () => {
     );
 }
 
-const add_new_contact =async ()=> await db.run(`INSERT INTO users(name, email) VALUES(?, ?)`, ["Alice", "alice@example.com"]);
-const get_all_users =async ()=> await db.all(`SELECT * FROM users`);
-// console.log(get_all_users());
-const close_db =async ()=> await db.close();
+const add_new_contact = async () => await db.run(`INSERT INTO users(name, email) VALUES(?, ?)`, [prompt("your name?"), prompt("your email?")]);
 
-const main=()=>{
+const get_all_users = async () => {
+    const all_users = await new Promise((resolve, reject) => {
+        db.all("SELECT * FROM users", (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        });
+    });
+    console.log(all_users);
+};
 
+// const get_all_users = async () => {
+//   const all_users = await db.all(`SELECT * FROM users`);
+//   console.log(all_users);
+// };
+
+
+const close_db = async () => await db.close();
+
+const main = async () => {
+    init_table()
+    // CRUD
+    while (true) {
+        const user_selection = menu();
+        if (user_selection == Selctions.EXIT) { return }
+        if (user_selection == Selctions.ADD) add_new_contact()
+        if (user_selection == Selctions.DISPLAY) await get_all_users()
+    }
 }
 main()
